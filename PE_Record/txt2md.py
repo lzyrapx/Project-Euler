bcf7944938b1ed706d6bab9df786e6e5a5c512ec
liangzhaoyang <lzyrapx@gmail.com>
2021-01-20 13:32:25 +0800

    add pe record

---
+++ PE_Record/txt2md.py
@@ -0,0 +1,28 @@
+
+history_filename = "lzyrapx_history.txt"
+
+with open(history_filename) as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+
+markdown_filename = "README.md"
+
+# clear all content
+with open(markdown_filename, 'r+') as f:
+    res = f.readlines()
+    f.seek(0)
+    f.truncate()
+
+# write new context
+writer = open(markdown_filename,'w')
+writer.write("|Problem ID|Date|time|\n")
+writer.write("|------:|---:|------:|\n")
+for line in content:
+    problem_id = line.split(" ")[0]
+    id = problem_id[0:len(problem_id)-1]
+    date = line.split(" ")[1] + " " + line.split(" ")[2] + " " + line.split(" ")[3]
+    time = line.split(" ")[4]
+    writer.write("|" + id + "|" + date + "|" + time + "|\n")
+writer.close()
+
